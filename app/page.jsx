43d293f154c7d31eaa5261af9b95794be75a5f59
Kocayamak <mikailkocayamak@gmail.com>
2023-08-11 20:05:57 +0300

    RoorLayout craeted

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Keşfet & Paylaş
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          Yapay Zeka Gücü Aramalar
+        </span>
+      </h1>
+
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quibusdam
+        soluta debitis cupiditate explicabo quae a ad quasi blanditiis,
+      </p>
+
+      
+    </section>
+  );
+};
+
+export default Home;
